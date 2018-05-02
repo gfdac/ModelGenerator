720d2814bebc9980d662900b0dfa60a0dc794308
pandabr <guh.clemente@gmail.com>
2018-05-01 21:05:40 -0300

    Laravel exporter model avaliable

--- exporter.py
+++ exporter.py
@@ -3,7 +3,7 @@
 import os
 
 tipos = ["String", "Boolean", "Integer", "Number", "Float", "Array", "Object", "Others..."]
-exporters = ["Javascript CommonJS", "Javascript Simples", "Java", "TypeScript", "Swift", "PHP", "Todos"]
+exporters = ["Javascript CommonJS", "Javascript Simples", "Java", "TypeScript", "Swift", "PHP", 'Laravel', "Todos"]
 
 # CONSTANTS for exportes and Tipos
 K_TIPO_STRING = 'String'
@@ -24,7 +24,8 @@ K_EXPORTER_JAVA = 2
 K_EXPORTER_TYPESCRIPT = 3
 K_EXPORTER_SWIFT = 4
 K_EXPORTER_PHP = 5
-K_EXPORTER_TODOS = 6
+K_EXPORTER_PHP_LARAVEL = 6
+K_EXPORTER_TODOS = 7
 
 
 class bcolors:
@@ -119,6 +120,9 @@ def gerarExport(modelo):
     # php exporter
     elif exportar == K_EXPORTER_PHP:
         phpExporter(modelo)
+    # laravel exporter
+    elif exportar == K_EXPORTER_PHP_LARAVEL:
+        laravelExporter(modelo)
 
     # Todos
     elif exportar == K_EXPORTER_TODOS:
@@ -127,6 +131,7 @@ def gerarExport(modelo):
         javaExporter(modelo)
         swiftExporter(modelo)
         phpExporter(modelo)
+        laravelExporter(modelo)
     else:
         print("Other languages types soon")
         gerarExport(modelo)
@@ -179,15 +184,15 @@ def createPropertyTypeScript(p):
     r = ""
     # Strings
     if p.tipo is not None and p.valor is not None and p.valor is not "" and (
-                        p.tipo.lower() == K_TIPO_STRING.lower() or p.tipo.lower() == K_TIPO_TEXT.lower() or p.tipo.lower() == K_TIPO_VARCHAR.lower()):
+            p.tipo.lower() == K_TIPO_STRING.lower() or p.tipo.lower() == K_TIPO_TEXT.lower() or p.tipo.lower() == K_TIPO_VARCHAR.lower()):
         r = r + "     " + p.name + ": " + converteTipos(p.tipo, K_EXPORTER_TYPESCRIPT) + " = '" + p.valor + "';"
     # Numbers
     elif p.tipo is not None and p.valor is not None and p.valor is not "" and (
-                            p.tipo.lower() == K_TIPO_NUMBER.lower() or p.tipo.lower() == K_TIPO_FLOAT.lower() or p.tipo.lower() == K_TIPO_INTEGER.lower() or p.tipo.lower() == K_TIPO_DECIMAL.lower()):
+            p.tipo.lower() == K_TIPO_NUMBER.lower() or p.tipo.lower() == K_TIPO_FLOAT.lower() or p.tipo.lower() == K_TIPO_INTEGER.lower() or p.tipo.lower() == K_TIPO_DECIMAL.lower()):
         r = r + "     " + p.name + ": " + converteTipos(p.tipo, K_EXPORTER_TYPESCRIPT) + " = " + p.valor + ";"
     # Boolean
     elif p.tipo is not None and p.valor is not None and p.valor is not "" and (
-                p.tipo.lower() == K_TIPO_BOOLEAN.lower()):
+            p.tipo.lower() == K_TIPO_BOOLEAN.lower()):
         r = r + "     " + p.name + ": " + converteTipos(p.tipo, K_EXPORTER_TYPESCRIPT) + " = " + p.valor + ";"
     elif p.tipo is not None:
         r = r + "     " + p.name + ": " + converteTipos(p.tipo, K_EXPORTER_TYPESCRIPT) + " = null;"
@@ -200,15 +205,15 @@ def createPropertySwift(p):
     r = ""
     # Strings
     if p.tipo is not None and p.valor is not None and p.valor is not "" and (
-                        p.tipo.lower() == K_TIPO_STRING.lower() or p.tipo.lower() == K_TIPO_TEXT.lower() or p.tipo.lower() == K_TIPO_VARCHAR.lower()):
+            p.tipo.lower() == K_TIPO_STRING.lower() or p.tipo.lower() == K_TIPO_TEXT.lower() or p.tipo.lower() == K_TIPO_VARCHAR.lower()):
         r = r + "     var " + p.name + ": " + converteTipos(p.tipo, K_EXPORTER_SWIFT) + " = '" + p.valor + "';"
     # Numbers
     elif p.tipo is not None and p.valor is not None and p.valor is not "" and (
-                            p.tipo.lower() == K_TIPO_NUMBER.lower() or p.tipo.lower() == K_TIPO_FLOAT.lower() or p.tipo.lower() == K_TIPO_INTEGER.lower() or p.tipo.lower() == K_TIPO_DECIMAL.lower()):
+            p.tipo.lower() == K_TIPO_NUMBER.lower() or p.tipo.lower() == K_TIPO_FLOAT.lower() or p.tipo.lower() == K_TIPO_INTEGER.lower() or p.tipo.lower() == K_TIPO_DECIMAL.lower()):
         r = r + "     var " + p.name + ": " + converteTipos(p.tipo, K_EXPORTER_SWIFT) + " = " + p.valor + ";"
     # Boolean
     elif p.tipo is not None and p.valor is not None and p.valor is not "" and (
-                p.tipo.lower() == K_TIPO_BOOLEAN.lower()):
+            p.tipo.lower() == K_TIPO_BOOLEAN.lower()):
         r = r + "     var " + p.name + ": " + converteTipos(p.tipo, K_EXPORTER_SWIFT) + " = " + p.valor + ";"
     elif p.tipo is not None:
         r = r + "     var " + p.name + ": " + converteTipos(p.tipo, K_EXPORTER_SWIFT) + " = nil;"
@@ -241,6 +246,20 @@ def createPropertyPHP(p):
         r = r + "     public $_" + p.name + " = null;"
     return r
 
+#protected $fillable = ['nome', ...];
+def createPropertyLaravel(modelo):
+
+    r = ""
+
+    for p in modelo.propriedades:
+        r = r + "'"  + p.name + "'" + ","
+
+    r = r.rstrip(',')
+
+    r = "   protected $fillable = [" + r + "];"
+
+    return r
+
 
 def createDefinePropertyCommonJS(p):
     r = ""
@@ -264,13 +283,13 @@ def createMetodhTypeScript(m):
     for param in m.parametros:
 
         if param.tipo is not None and param.tipo is not "" and param.valor is not None and param.valor is not "" and (
-                            param.tipo.lower() == K_TIPO_STRING.lower() or param.tipo.lower() == K_TIPO_TEXT.lower() or param.tipo.lower() == K_TIPO_VARCHAR.lower()):
+                param.tipo.lower() == K_TIPO_STRING.lower() or param.tipo.lower() == K_TIPO_TEXT.lower() or param.tipo.lower() == K_TIPO_VARCHAR.lower()):
             r = r + param.name + ": " + converteTipos(param.tipo,
                                                       K_EXPORTER_TYPESCRIPT) + " = '" + str(
                 param.valor) + "', "
 
         elif param.tipo is not None and param.tipo is not "" and param.valor is not None and param.valor is not "" and (
-                                param.tipo.lower() == K_TIPO_DECIMAL.lower() or param.tipo.lower() == K_TIPO_INTEGER.lower() or param.tipo.lower() == K_TIPO_NUMBER.lower() or param.tipo.lower() == K_TIPO_FLOAT.lower()):
+                param.tipo.lower() == K_TIPO_DECIMAL.lower() or param.tipo.lower() == K_TIPO_INTEGER.lower() or param.tipo.lower() == K_TIPO_NUMBER.lower() or param.tipo.lower() == K_TIPO_FLOAT.lower()):
             r = r + param.name + ": " + converteTipos(param.tipo,
                                                       K_EXPORTER_TYPESCRIPT) + " = " + str(
                 param.valor) + ", "
@@ -291,12 +310,12 @@ def createMetodhSwift(m):
     for param in m.parametros:
 
         if param.tipo is not None and param.tipo is not "" and param.valor is not None and param.valor is not "" and (
-                            param.tipo.lower() == K_TIPO_STRING.lower() or param.tipo.lower() == K_TIPO_TEXT.lower() or param.tipo.lower() == K_TIPO_VARCHAR.lower()):
+                param.tipo.lower() == K_TIPO_STRING.lower() or param.tipo.lower() == K_TIPO_TEXT.lower() or param.tipo.lower() == K_TIPO_VARCHAR.lower()):
             r = r + param.name + ": " + converteTipos(param.tipo,
                                                       K_EXPORTER_SWIFT) + "', "
 
         elif param.tipo is not None and param.tipo is not "" and param.valor is not None and param.valor is not "" and (
-                                param.tipo.lower() == K_TIPO_DECIMAL.lower() or param.tipo.lower() == K_TIPO_INTEGER.lower() or param.tipo.lower() == K_TIPO_NUMBER.lower() or param.tipo.lower() == K_TIPO_FLOAT.lower()):
+                param.tipo.lower() == K_TIPO_DECIMAL.lower() or param.tipo.lower() == K_TIPO_INTEGER.lower() or param.tipo.lower() == K_TIPO_NUMBER.lower() or param.tipo.lower() == K_TIPO_FLOAT.lower()):
             r = r + param.name + ": " + converteTipos(param.tipo,
                                                       K_EXPORTER_SWIFT) + ", "
 
@@ -323,6 +342,15 @@ def createMetodhsCommonJS(m):
 #        // stuff
 #    }
 def createMetodhsPHP(m):
+    parametros = ""
+
+    for param in m.parametros:
+        parametros = parametros + "$_" + param.name + ", "
+
+    return "     public function " + m.name + "(" + parametros.rstrip(', ') + "){}" + "\n"
+
+
+def createMetodhsLaravel(m):
     parametros = ""
 
     for param in m.parametros:
@@ -425,7 +453,7 @@ def swiftExporter(modelo):
 def phpExporter(modelo):
     print('*' * 50)
     # r = result
-    r = ""
+    r = "<?php"
     r = r + "class " + modelo.name + " extends Model {" + "\n"
 
     r = r + "   public function __construct() {" + "\n"
@@ -442,6 +470,35 @@ def phpExporter(modelo):
     print(r)
     print('*' * 50)
     write_file(modelo.name + "_php.php", r)
+
+
+def laravelExporter(modelo):
+    print('*' * 50)
+    # r = result
+    r = "<?php" + "\n"
+    r = r + "namespace App;" + "\n"
+    r = r + "use Illuminate\Database\Eloquent\Model;" + "\n"
+    r = r + "class " + modelo.name + " extends Model {" + "\n"
+
+    #  protected $table = 'fornecedores';
+    r = r + "   protected $table = '" + modelo.name + "';" + "\n"
+    r = r + "   public $timestamps = false;" + "\n"
+
+    r = r + createPropertyLaravel(modelo) + "\n"
+
+    for m in modelo.metodos:
+        r = r + createMetodhsLaravel(m) + "\n"
+
+
+    r = r + "   public function __construct() {" + "\n"
+    # $this->_conn = $connection;
+    r = r + "   }" + "\n"
+
+    r = r + "}\n"
+
+    print(r)
+    print('*' * 50)
+    write_file(modelo.name + "_laravel.php", r)
 
 
 def javaExporter(modelo):
